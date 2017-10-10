451b2c53654e7873f783c503628615fc75dea195
Jan Mercl <0xjnml@gmail.com>
2017-10-10 19:38:00 +0200

    Fix off by one order typo. (again)
    	modified:   99prof/README.md
    	modified:   99prof/main.go
    	modified:   README.md

--- 99prof/main.go
+++ 99prof/main.go
@@ -260,7 +260,7 @@
 //     $
 //
 // Alternatively, using 99dump, we can see that the loop consists of 8
-// instructions at addresses 0x00044-0x0004b. 34900000*8 = 27920000 confirming
+// instructions at addresses 0x00044-0x0004b. 34900000*8 = 279200000 confirming
 // the estimated ~279MIPS value.
 package main
 
